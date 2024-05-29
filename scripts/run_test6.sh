2f5c3d92c3d83a6277efe7a69ecf237a40d12fc7
harryaddlesee <40343409+harryaddlesee@users.noreply.github.com>
2024-05-29 11:22:40 +0100

    Update run_test6.sh

--- scripts/run_test6.sh
+++ scripts/run_test6.sh
@@ -3,6 +3,7 @@
 #SBATCH --job-name=singlecputasks
 #SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
+#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
 
 # Your script goes here
 srun --ntasks=1 echo "I'm task 1"
