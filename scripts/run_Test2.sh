feba83e4f511a9b11e142d1a31db050d9d137417
harryaddlesee <40343409+harryaddlesee@users.noreply.github.com>
2024-05-29 10:23:40 +0100

    Update run_Test2.sh

--- scripts/run_Test2.sh
+++ scripts/run_Test2.sh
@@ -5,18 +5,20 @@
 #SBATCH --time=01:00:00                   # Wall time limit (hh:mm:ss)
 #SBATCH --nodes=1                         # Number of nodes
 #SBATCH --ntasks=1                        # Number of tasks (processes)
-#SBATCH --cpus-per-task=4                 # Number of CPU cores per task
+#SBATCH --cpus-per-task=2                 # Number of CPU cores per task
 #SBATCH --mem=4GB                         # Total memory limit
 #SBATCH --partition=gpu                   # Partition name (adjust as necessary)
+#SBATCH --gres=gpu:2                      # Request 2 GPUs
 
-# Load any required modules (if needed)
-module load anaconda/2020.07  # Example of loading Anaconda module (adjust as necessary)
+# Load any required modules
+module load anaconda/2020.07
 
 # Activate your conda environment
 source activate myenv
 
 # Run your Python script
-python /scripts/Test1.py
+#python /scripts/Test1.py
+srun --cpus-per-task=2 python ./Test1.py &> combined_log_%j.txt
 
 # Deactivate the conda environment
 conda deactivate
