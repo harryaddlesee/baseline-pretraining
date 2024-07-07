5237e54309fe2705362c50a71d11973022907215
Harry Addlesee <ha2098@login1.pri.dmog.alces.network>
2024-07-07 19:51:09 +0100

    updating training branch

--- scripts/training.sh
+++ scripts/training.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=preJOB  
 #SBATCH --output=output_%j.txt     
 #SBATCH --error=error_%j.txt              
-#SBATCH --time=01:00:00             
+#SBATCH --time=03:00:00             
 #SBATCH --nodes=1                  
 #SBATCH --ntasks=1                      
 #SBATCH --cpus-per-task=2                
@@ -11,5 +11,5 @@
 #SBATCH --gres=gpu:1
 
 # Run your Python script
-# export BABYLM_ROOT_DIR=/users/ha2098/sharedscratch/venv/projects/baseline-pretraining/trainDir
+export BABYLM_ROOT_DIR=/users/ha2098/sharedscratch/venv/projects/baseline-pretraining/trainDir
 python scripts/general_train.py --output_dir trainDir/model_checkpoints --do_train
