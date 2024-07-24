637392d2c12e0d9a850623b445880a24c3d700e3
harryaddlesee <40343409+harryaddlesee@users.noreply.github.com>
2024-07-24 11:51:40 +0100

    Update hf_loaders.py

--- src/babylm_baseline_train/datasets/hf_loaders.py
+++ src/babylm_baseline_train/datasets/hf_loaders.py
@@ -9,7 +9,8 @@ repo_path = babylm_baseline_train.__path__[0]
 def get_babyLM(name, split):
     dataset = load_dataset(
             path=os.path.join(
-                repo_path, 'datasets', "babyLM_for_hf.py"),
+                repo_path, 'datasets', "babyLM_for_hf2.py"),
+                #repo_path, 'datasets', "babyLM_for_hf.py"),
             name=name,
             split=split)
     return dataset
